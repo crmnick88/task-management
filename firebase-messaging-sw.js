3faa79bae66276c77856e7f5720d3b746ce3cb89
crmnick88 <crmnick88@gmail.com>
2026-02-07 11:23:35 +0200

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC2gl8SbTx81sZsXPjctfEesfn7P2kKlKk",
+    authDomain: "task-management-2beb8.firebaseapp.com",
+    projectId: "task-management-2beb8",
+    storageBucket: "task-management-2beb8.firebasestorage.app",
+    messagingSenderId: "100466346989",
+    appId: "1:100466346989:web:7344dded39f76143b0702c"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('Received background message:', payload);
+    
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/icon.png'
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
